4bcfdcc4d5f2a47cb6fa337c4ec747d511ba94f5
Aditya <90451018+realAdif@users.noreply.github.com>
2022-07-31 19:08:33 +1000

    port fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 let termData = require('./db/db.json');
 const app = express();
 
